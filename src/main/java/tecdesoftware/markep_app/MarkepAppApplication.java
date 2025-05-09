88caa83dd2825450e64d48dec108fe7af793f06a
TuNombreDeUsuario <joel.diaz.lopez7@gmnail.com>
2025-05-09 11:38:37 -0600

    Adding HelloController to String

---
+++ src/main/java/tecdesoftware/markep_app/MarkepAppApplication.java
@@ -0,0 +1,13 @@
+package tecdesoftware.markep_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkepAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkepAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tecdesoftware/markep_app/MarkepAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tecdesoftware.markep_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkepAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
